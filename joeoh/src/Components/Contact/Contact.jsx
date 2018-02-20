9bec388ea15e60804bb488c0e279dd348a2ff835
unknown <danoh82@gmail.com>
2018-02-19 23:11:38 -0800

    updated images and some css

--- joeoh/src/Components/Contact/Contact.jsx
+++ joeoh/src/Components/Contact/Contact.jsx
@@ -10,6 +10,9 @@ import {
     Segment,
 } from 'semantic-ui-react';
 import Map from './GMap/GMap';
+import Kaist from '../../Images/kaist.jpg';
+import Ucsc from '../../Images/Ucsc.png';
+
 //Google Maps api key : AIzaSyB4Gae_gW6rhT4AKAxkr13R-4uolR1kZD8
 
 
@@ -30,6 +33,14 @@ const Intro = (props) => {
                         <Grid.Column width={10}>
                             <h1>학력</h1>
                             <h1>이력</h1>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image src={Kaist}/>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Image src={Ucsc}/>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
